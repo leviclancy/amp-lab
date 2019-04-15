cf7b6a7f21cfc40b039a23be41b8af72726a64f0
Levi Clancy <32032952+leviclancy@users.noreply.github.com>
2019-04-15 14:44:36 +0900

    Create index.php

---
+++ index.php
@@ -0,0 +1,64 @@
+<? file
+
+<!-- Open document -->
+<!doctype html> <html amp lang='en'>
+
+<!-- Open HTML head -->
+<head> <meta charset='utf-8'>
+
+<!-- AMP JS -->
+<script async src='https://cdn.ampproject.org/v0.js'></script>
+
+<!-- Must define canonical url for amp -->
+<link rel='canonical' href='https://amp-lab.narrative.fyi>
+
+<!-- AMP boilerplate code -->
+<style amp-boilerplate>body{-webkit-animation:-amp-start 8s steps(1,end) 0s 1 normal both;-moz-animation:-amp-start 8s steps(1,end) 0s 1 normal both;-ms-animation:-amp-start 8s steps(1,end) 0s 1 normal both;animation:-amp-start 8s steps(1,end) 0s 1 normal both}@-webkit-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-moz-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-ms-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-o-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}</style><noscript><style amp-boilerplate>body{-webkit-animation:none;-moz-animation:none;-ms-animation:none;animation:none}</style></noscript>
+
+<!-- Define viewport for AMP -->
+<meta name='viewport' content='width=device-width,minimum-scale=1,initial-scale=1'>
+
+<!-- AMP elements -->
+<script async custom-element="amp-form" src="https://cdn.ampproject.org/v0/amp-form-0.1.js"></script> <!-- AMP form -->
+<script async custom-element="amp-bind" src="https://cdn.ampproject.org/v0/amp-bind-0.1.js"></script> <!-- AMP bind -->
+<script async custom-element="amp-fx-collection" src="https://cdn.ampproject.org/v0/amp-fx-collection-0.1.js"></script> <!-- AMP parallax -->
+	
+<title>AMP Lab @narrative.fyi</title>
+<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+<meta name='theme-color' content='#000'>
+<base href='/' />
+
+<style amp-custom>
+	<? include_once('style.css'); ?>
+	</style>
+
+</head>
+	
+<body>
+	
+<amp-state id=’translatableElements’>
+
+	</amp-state>
+
+
+<h1>AMP Lab</h1>
+
+<p>Chonan! (Kurdish <i>Hello, folks</i></i>) We are excited to show you live examples of the subject matter in our talk from 2019's Google AMP conference. Check it out below, and get in touch for any questions. Contact information is avaialable at <a href-"https://narrative.fyi">narrative.fyi</a>.</p>
+
+<h2>Language switching!</h2>
+
+<p>Language is one of the most fundamental boundaries for many war-impacted people, who often have the least access to English-learning institutions.</p>
+
+// buttons
+
+<div>
+
+Home
+
+Sign in
+
+</div>
+
+<h2>AMP listing whenever possible!</h2>
+
+<h2>AMP form responsiveness</h2>
