058b3f731213bea21fa7998373cae90abe1a3467
Levi Clancy <32032952+leviclancy@users.noreply.github.com>
2019-04-15 17:41:01 +0900

    Update index.php

--- index.php
+++ index.php
@@ -96,11 +96,12 @@ $text_direction_default = "ltr"; ?>
 <span class='material-icons' role='button' tabindex='0' on="tap: AMP.setState({selectedDirection: 'left-to-right', selectedLanguage: selectedLanguage || '<? echo $language_default ?>'})">format_textdirection_l_to_r</span>
 <span class='material-icons' role='button' tabindex='0' on="tap: AMP.setState({selectedDirection: 'right-to-left', selectedLanguage: selectedLanguage || '<? echo $language_default ?>'})">format_textdirection_r_to_l</span>
 </div>
-
-<span id='home-button' class='<? echo $text_direction_default ?>' [class]="selectedDirection" [text]="translatableElements['home'][selectedLanguage]"><? echo $translatable_elements['home'][$language_default] ?></span>
 	
+<span id='button-alignment'>
+<span id='home-button' class='<? echo $text_direction_default ?>' [class]="selectedDirection" [text]="translatableElements['home'][selectedLanguage]"><? echo $translatable_elements['home'][$language_default] ?></span>	
 <span id='sign-in-button' class='<? echo $text_direction_default ?>' [class]="selectedDirection" [text]="translatableElements['sign-in'][selectedLanguage]"><? echo $translatable_elements['sign-in'][$language_default] ?></span>
-	
+</span>
+
 <p class='<? echo $text_direction_default ?>' [class]="selectedDirection" [text]="translatableElements['language-paragraph-two'][selectedLanguage]"><? echo $translatable_elements['language-paragraph-two'][$language_default] ?></p>
 
 </div>
